8b58f913b3f945464bd28bbb2fcc158317f4e395
Gor Hayrapetyan <ghayrapetyan890@gmail.com>
2022-09-18 18:05:41 +0400

    deleted lesson3_1 folder and files

--- lesson3_1/lesson3_3.py
+++
@@ -1,6 +0,0 @@
-#Write a Python program to remove the n-th index character from a nonempty string.
-
-userWord = input("Write the word you want to change: ")
-userResponse = int(input("Write the index number of the letter which you want to remove: ")) 
-
-print(userWord[:userResponse] + userWord[userResponse + 1:])
